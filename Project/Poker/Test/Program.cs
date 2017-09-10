0cb32f33f1322bdd867d51fc5d07b4c1dd3e9fae
nevena94rad <nensy94ns@gmail.com>
2017-09-10 23:19:52 +0200

    nesto

--- Project/Poker/Test/Program.cs
+++ Project/Poker/Test/Program.cs
@@ -15,28 +15,22 @@ namespace Test
             string board = "2h 2c 5c 7c jh";
             Hand player1 = new Hand("5h 7h", board);
             Hand player2 = new Hand("jc ah", board);
-
+            Hand player3 = new Hand("2d 2s", board);
             // Print out a hand description: in this case both hands are
             // A straight, six high
             Console.WriteLine("Player1 Hand: {0}", player1.Description);
             Console.WriteLine("Player2 Hand: {0}", player2.Description);
+            Console.WriteLine("Player3 Hand: {0}", player3.Description);
 
-            // Compare hands
-            if (player1 == player2)
-            {
-                Console.WriteLine("player1's hand is" +
-                                  " equal to player2's hand");
-            }
-            else if (player1 > player2)
-            {
-                Console.WriteLine("player1's hand is " +
-                                  "greater than player2's hand");
-            }
-            else
-            {
-                Console.WriteLine("player2's hand is greater" +
-                                  " than or equal player1's hand");
-            }
+
+
+            Hand[] hands = new Hand[3];
+            hands[0] = player1;
+            hands[1] = player2;
+            hands[2] = player3;
+            hands = hands.OrderByDescending(x => x.HandValue).ToArray();
+            Console.WriteLine(hands[0].Description);
+         
         }
     }
 }
