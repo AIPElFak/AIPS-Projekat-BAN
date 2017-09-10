2e21a6ebc983ecbdb6e3ef63a7f3bcbb0ff23ceb
Aleksandar Stojcic <alexstojcic@elfak.rs>
2017-09-10 22:46:11 +0200

    Sitnice za dugmice

--- Project/Poker/Poker/Scripts/buttons.js
+++ Project/Poker/Poker/Scripts/buttons.js
@@ -16,8 +16,13 @@ var raise = function () {
 }
 
 var disableButtons = function () {
-
+    document.getElementById("raise").disabled = true;
+    document.getElementById("call").disabled = true;
+    document.getElementById("fold").disabled = true;
 }
 var enableButtons = function () {
 
+    document.getElementById("raise").disabled = false;
+    document.getElementById("call").disabled = false;
+    document.getElementById("fold").disabled = false;
 }
